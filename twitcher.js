1e124e019ad740bbfe54ac6c6a9522ed5a45ea33
Bradley Rumack <bradleyrumack@Bradleys-MacBook-Pro.local>
2017-05-06 14:00:07 -0700

    added css and js

---
+++ twitcher.js
@@ -0,0 +1,76 @@
+$("document").ready(function() {
+
+
+  var getUserData = function(userName, userNumber) {
+    var key = "n9w785u28g4g9thqa7qm7q0d9ee8qz";
+    $.ajax({
+      url:'https://api.twitch.tv/channels/' + userName +'?oauth_token=' + key + '&callback=?',
+      dataType: "json",
+      type: "GET",
+      success: function(data) {
+        console.log(data);
+        if (data.video_banner != null) {
+          $(".content").append(
+            '<div class = "row"><span class = "user"><img id = "img' + userNumber + '" src="' +
+              data.video_banner +
+              '"><a href = "https://www.twitch.tv/' +
+              data.name +
+              '" target="_blank"><span class = "tray" id = "user' +
+              userNumber +
+              '"></span></a></span></div>'
+          );
+
+          $("#user" + userNumber).append(
+            "<H1>" + data.display_name + "</a></H1><H3>" + data.status + "</H3>"
+          );
+          if (data.game != null)
+            $("#user" + userNumber).append(
+              "<H5>Playing: " + data.game + "</H5>"
+            );
+
+          $(".tray").css("height", $("img").height());
+
+          $.ajax({
+            url:'https://api.twitch.tv/kraken/streams/' + userName + '?oauth_token=' + key + '&callback=?',
+            dataType: "json",
+            type: "GET",
+            success: function(data) {
+
+            if (data.stream === null)
+              $("#img" + userNumber).css("box-shadow", "0px 0px 54px red" );
+            else
+              $("#img" + userNumber).css("box-shadow", "0px 0px 40px green" );
+            }
+
+});
+
+
+
+
+        }
+      }
+    });
+  };
+
+  var userArray = [
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "freecodecamp",
+    "storbeck",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas"
+  ];
+  $(".tray").css("background", "#0e9dd9");
+  $(window).load(function () {
+    $('#mask').fadeOut('slow');
+});
+
+
+
+  for (var i = 0; i < userArray.length; i++) getUserData(userArray[i], i);
+  window.onresize = function(){ location.reload(); }
+
+
+});
